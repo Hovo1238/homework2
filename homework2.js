1560707a00b4a88d59bef4ef3373802cbd0cc68b
--reset <--reset>
2018-09-11 19:23:02 +0400

    first commit

---
+++ homework2.js
@@ -0,0 +1,13 @@
+const a = 10;
+const b = 20;
+
+const c = a + b;
+const d = a / b;
+
+const name = "Hovhannes";
+
+if(c / d === 60)
+	console.log(c + " \\ " + d + " = 60");
+else
+	console.log(c + " \\ " + d + " != 60");
+
